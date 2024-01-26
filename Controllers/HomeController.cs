9285411d9b47498772940a934ce9c74166eb9b80
justinJSwebdev <trankyhung225@gmail.com>
2024-01-26 20:14:18 +0700

    first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace b.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/hello")]
+        public IActionResult Index()
+        {
+            return Json("Hello");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+namespace b
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.MapControllers(); //useRouting + use Endpoints
+            app.UseAuthorization();
+           
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
